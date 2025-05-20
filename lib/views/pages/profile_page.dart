d7b31e8cf67195ad5a7fec5fe16b39352606c37f
Amphikapha <amphikapha@gmail.com>
2025-05-20 15:33:03 +0700

    checkbox

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -9,6 +9,7 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   TextEditingController nameController = TextEditingController();
+  bool? isChecked = false;
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +27,25 @@ class _ProfilePageState extends State<ProfilePage> {
             onEditingComplete:() => setState(() {}),
           ),
           Text(nameController.text),
-          
+          Checkbox(
+            tristate: true,
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value;
+              });
+            },
+          ),
+          CheckboxListTile(
+            tristate: true,
+            title: const Text('Check me'),
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value;
+              });
+            },
+          ),
         ]
       ),
     );
