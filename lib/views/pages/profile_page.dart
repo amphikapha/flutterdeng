5f67848d92c4dedbbe4f6b554b89fb9f33463d0a
Amphikapha <amphikapha@gmail.com>
2025-05-20 15:49:19 +0700

    SingleChildScrollView

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -15,59 +15,63 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          TextField(
-            controller: nameController,
-            decoration: InputDecoration(
-              labelText: 'Name',
-              border: OutlineInputBorder(),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                labelText: 'Name',
+                border: OutlineInputBorder(),
+              ),
+              onEditingComplete:() => setState(() {}),
             ),
-            onEditingComplete:() => setState(() {}),
-          ),
-          Text(nameController.text),
-          Checkbox(
-            tristate: true,
-            value: isChecked,
-            onChanged: (bool? value) {
-              setState(() {
-                isChecked = value;
-              });
-            },
-          ),
-          CheckboxListTile.adaptive(
-            tristate: true,
-            title: const Text('Check me'),
-            value: isChecked,
-            onChanged: (bool? value) {
-              setState(() {
-                isChecked = value;
-              });
-            },
-          ),
-          Switch(value: isSwitched, onChanged:(value) => setState(() {
-            isSwitched = value;
-          }),),
-          SwitchListTile.adaptive(
-            title: const Text('Switch me'),
-            value: isSwitched,
-            onChanged: (bool value) {
-              setState(() {
-                isSwitched = value;
-              });
-            },
-          ),
-          Slider.adaptive(
-            max: 10.0,
-            divisions: 10,
-            value: sliderValue, 
-            onChanged:(value) => setState(() {
-            sliderValue = value;
-          }),),
-        ]
+            Text(nameController.text),
+            Checkbox(
+              tristate: true,
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            CheckboxListTile.adaptive(
+              tristate: true,
+              title: const Text('Check me'),
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            Switch(value: isSwitched, onChanged:(value) => setState(() {
+              isSwitched = value;
+            }),),
+            SwitchListTile.adaptive(
+              title: const Text('Switch me'),
+              value: isSwitched,
+              onChanged: (bool value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+            Slider.adaptive(
+              max: 10.0,
+              divisions: 10,
+              value: sliderValue, 
+              onChanged:(value) => setState(() {
+              sliderValue = value;
+            }),), 
+            Image.asset('assets/images/macos.jpg'),
+            Image.asset('assets/images/macos.jpg'),
+          ]
+        ),
       ),
     );
   }
