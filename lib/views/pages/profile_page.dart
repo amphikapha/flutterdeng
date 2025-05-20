b478dc25a2f7ce55d1152154db8f1e194586cae0
Amphikapha <amphikapha@gmail.com>
2025-05-20 15:32:42 +0700

    textfield

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  TextEditingController nameController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text('profile'),);
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextField(
+            controller: nameController,
+            decoration: InputDecoration(
+              labelText: 'Name',
+              border: OutlineInputBorder(),
+            ),
+            onEditingComplete:() => setState(() {}),
+          ),
+          Text(nameController.text),
+          
+        ]
+      ),
+    );
   }
 }
