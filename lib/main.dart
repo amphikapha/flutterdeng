c01b41618f9dca63a660403db58baacfcc804c92
Amphikapha <amphikapha@gmail.com>
2025-05-21 14:36:19 +0700

    shared preference to save data locally

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutterdeng/data/constant.dart';
 import 'package:flutterdeng/data/notifiers.dart';
 import 'package:flutterdeng/views/pages/welcome_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,6 +20,18 @@ class _MyAppState extends State<MyApp> {
   // ถ้าอยู่ใน build จะสร้างใหม่ทุกครั้งที่มีการ rebuild
   // int currentIndex = 0; ย้ายไปไว้ใน NavBarWidget เพราะจะได้ rebuild แค่ NavBarWidget
   // ถ้าอยู่ใน build จะสร้างใหม่ทุกครั้งที่มีการ rebuild
+
+  @override
+  void initState() {
+    initThemeMode();
+    super.initState();
+  }
+
+  void initThemeMode() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final bool? themeMode = prefs.getBool(KConstants.themeModeKey);
+    isDarkModeNotifier.value = themeMode ?? false;
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -1,55 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutterdeng/data/constant.dart';
 import 'package:flutterdeng/data/notifiers.dart';
 import 'package:flutterdeng/views/pages/home_page.dart';
 import 'package:flutterdeng/views/pages/profile_page.dart';
 import 'package:flutterdeng/views/pages/setting_page.dart';
 import 'package:flutterdeng/widgets/navbar_wiget.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-List<Widget> pages = [
-  HomePage(),
-  ProfilePage(),
-];
+List<Widget> pages = [HomePage(), ProfilePage()];
 
 class WidgetTree extends StatelessWidget {
   const WidgetTree({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Demo'),
-          actions: [
-            IconButton(
-              icon: ValueListenableBuilder(
-                valueListenable:  isDarkModeNotifier,
-                builder: (context, isDarkMode, child) {
-                  return  isDarkMode
-                      ? const Icon(Icons.light_mode)
-                      : const Icon(Icons.dark_mode);
-                },
-              ),
-              onPressed: () {
-                isDarkModeNotifier.value = !isDarkModeNotifier.value;
+      appBar: AppBar(
+        title: const Text('Flutter Demo'),
+        actions: [
+          IconButton(
+            icon: ValueListenableBuilder(
+              valueListenable: isDarkModeNotifier,
+              builder: (context, isDarkMode, child) {
+                return isDarkMode
+                    ? const Icon(Icons.light_mode)
+                    : const Icon(Icons.dark_mode);
               },
             ),
-            IconButton(
-              onPressed:() {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SettingPage(title: 'Setting eiei',),
-                  ),
-                );
-              }, 
-              icon: Icon(Icons.settings),)
-          ]
-        ),
-        bottomNavigationBar: NavBarWidget(),
-        body: ValueListenableBuilder(valueListenable: selectedPageNotifier, builder:(context, selectPage, child) {
-          return pages.elementAt(selectPage); // ใช้ currentIndex จาก NavBarWidget
+            onPressed: () async {
+              isDarkModeNotifier.value = !isDarkModeNotifier.value;
+              final SharedPreferences prefs =await SharedPreferences.getInstance();
+              await prefs.setBool(KConstants.themeModeKey, isDarkModeNotifier.value);
+            },
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder:
+                      (context) => const SettingPage(title: 'Setting eiei'),
+                ),
+              );
+            },
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      ),
+      bottomNavigationBar: NavBarWidget(),
+      body: ValueListenableBuilder(
+        valueListenable: selectedPageNotifier,
+        builder: (context, selectPage, child) {
+          return pages.elementAt(
+            selectPage,
+          ); // ใช้ currentIndex จาก NavBarWidget
         }, // ใช้ currentIndex จาก NavBarWidget
-      )
+      ),
     );
   }
 }
