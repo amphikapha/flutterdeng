f026a5ad77fe24d411e904f680ee5ffd00865679
Amphikapha <amphikapha@gmail.com>
2025-05-21 14:05:45 +0700

    pushAndRemoveUntil

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -3,17 +3,19 @@ import 'package:flutterdeng/views/widget_tree.dart';
 import 'package:flutterdeng/widgets/hero_widget.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({super.key, required this.title});
+
+  final String title;
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  TextEditingController controllerEmail = TextEditingController();
+  TextEditingController controllerEmail = TextEditingController(text: '000');
   TextEditingController controllerPassword = TextEditingController();
 
-  String confirmEmail = '123';
+  String confirmEmail = '000';
   String confirmPassword = '000';
 
   @override
@@ -27,43 +29,51 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            HeroWidget(title: 'Login'),
-            SizedBox(height: 20),
-            TextField(
-              controller: controllerEmail,
-              decoration: InputDecoration(
-                hintText: 'Email',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
+      appBar: AppBar(),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                HeroWidget(title: widget.title),
+                SizedBox(height: 20),
+                TextField(
+                  controller: controllerEmail,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onEditingComplete: () => setState(() {}),
                 ),
-              ),
-              onEditingComplete: () => setState(() {}),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              controller: controllerPassword,
-              decoration: InputDecoration(
-                hintText: 'Password',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
+                SizedBox(height: 10),
+                TextField(
+                  controller: controllerPassword,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onEditingComplete: () => setState(() {}),
                 ),
-              ),
-              onEditingComplete: () => setState(() {}),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  style: FilledButton.styleFrom(
+                    minimumSize: const Size(200, 45),
+                  ),
+                  onPressed: () {
+                    onLoginPressed();
+                  },
+                  child: Text(widget.title),
+                ),
+                SizedBox(height: 100),
+              ],
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              style: FilledButton.styleFrom(minimumSize: const Size(200, 45)),
-              onPressed: () {
-                onLoginPressed();
-              },
-              child: const Text('Login'),
-            ),
-          ],
+          ),
         ),
       ),
     );
@@ -72,11 +82,10 @@ class _LoginPageState extends State<LoginPage> {
   void onLoginPressed() {
     if (controllerEmail.text == confirmEmail &&
         controllerPassword.text == confirmPassword) {
-      Navigator.pushReplacement(
+      Navigator.pushAndRemoveUntil(
         context,
-        MaterialPageRoute(
-          builder: (context) => const WidgetTree(),
-        ),
+        MaterialPageRoute(builder: (context) => const WidgetTree()),
+        (route) => false,
       );
     } else {
       showDialog(
--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -8,49 +8,56 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FittedBox(
-            child: const Text(
-              'Welcome to Flutter Deng',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-          ),
-          const SizedBox(height: 20),
-          Padding(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
             padding: const EdgeInsets.all(20.0),
-            child: Lottie.asset(
-              'assets/lotties/welcome.json',
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FittedBox(
+                  child: const Text(
+                    'Welcome to Flutter Deng',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Lottie.asset(
+                    'assets/lotties/welcome.json',
+                  ),
+                ),
+                FilledButton(
+                  style: FilledButton.styleFrom(
+                    minimumSize: const Size(200, 45),
+                  ),
+                  onPressed: () {
+                    // Navigate to the next page
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) => const LoginPage(title: 'Register'),
+                    ));
+                  },
+                  child: const Text('Get Started'),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    minimumSize: const Size(200, 45),
+                  ),
+                  onPressed: () {
+                    // Navigate to the next page
+                    Navigator.push(context, MaterialPageRoute(
+                      builder:(context) {
+                        return LoginPage(title: 'Login');
+                      },
+                    ));
+                  },
+                  child: const Text('Login'),
+                ),
+              ],
             ),
           ),
-          FilledButton(
-            style: FilledButton.styleFrom(
-              minimumSize: const Size(200, 45),
-            ),
-            onPressed: () {
-              // Navigate to the next page
-              Navigator.push(context, MaterialPageRoute(
-                builder: (context) => const LoginPage(),
-              ));
-            },
-            child: const Text('Get Started'),
-          ),
-          TextButton(
-            style: TextButton.styleFrom(
-              minimumSize: const Size(200, 45),
-            ),
-            onPressed: () {
-              // Navigate to the next page
-              Navigator.push(context, MaterialPageRoute(
-                builder:(context) {
-                  return LoginPage();
-                },
-              ));
-            },
-            child: const Text('Login'),
-          ),
-        ],
+        ),
       ),
     );
   }
