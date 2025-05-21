7a7bb8728bcd0a3dc10dc93a859eb40f078e9c19
Amphikapha <amphikapha@gmail.com>
2025-05-21 12:23:30 +0700

    format document

--- lib/views/pages/setting_page.dart
+++ lib/views/pages/setting_page.dart
@@ -29,122 +29,135 @@ class _SettingPageState extends State<SettingPage> {
         // automaticallyImplyLeading: false,
       ),
       body: SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            DropdownButtonFormField(
-              value: menuItem,
-              items: [
-              DropdownMenuItem(
-                value: 'e1',
-                child: Text('e1'),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(children: [CloseButton(), BackButton()]),
+              DropdownButtonFormField(
+                value: menuItem,
+                items: [
+                  DropdownMenuItem(value: 'e1', child: Text('e1')),
+                  DropdownMenuItem(value: 'e2', child: Text('e2')),
+                  DropdownMenuItem(value: 'e3', child: Text('e3')),
+                ],
+                onChanged:
+                    (value) => setState(() {
+                      menuItem = value;
+                    }),
               ),
-              DropdownMenuItem(
-                value: 'e2',
-                child: Text('e2'),
+              TextField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  labelText: 'Name',
+                  border: OutlineInputBorder(),
+                ),
+                onEditingComplete: () => setState(() {}),
               ),
-              DropdownMenuItem(
-                value: 'e3',
-                child: Text('e3'),
-              ),
-            ], onChanged:(value) => setState(() {
-              menuItem = value;
-            })),
-            TextField(
-              controller: nameController,
-              decoration: InputDecoration(
-                labelText: 'Name',
-                border: OutlineInputBorder(),
-              ),
-              onEditingComplete:() => setState(() {}),
-            ),
-            Text(nameController.text),
-            Checkbox(
-              tristate: true,
-              value: isChecked,
-              onChanged: (bool? value) {
-                setState(() {
-                  isChecked = value;
-                });
-              },
-            ),
-            CheckboxListTile.adaptive(
-              tristate: true,
-              title: const Text('Check me'),
-              value: isChecked,
-              onChanged: (bool? value) {
-                setState(() {
-                  isChecked = value;
-                });
-              },
-            ),
-            Switch(value: isSwitched, onChanged:(value) => setState(() {
-              isSwitched = value;
-            }),),
-            SwitchListTile.adaptive(
-              title: const Text('Switch me'),
-              value: isSwitched,
-              onChanged: (bool value) {
-                setState(() {
-                  isSwitched = value;
-                });
-              },
-            ),
-            Slider.adaptive(
-              max: 10.0,
-              divisions: 10,
-              value: sliderValue, 
-              onChanged:(value) => setState(() {
-              sliderValue = value;
-            }),), 
-            InkWell(
-              splashColor: Colors.teal,
-              onTap:() {
-                print('Image tapped from InkWell');
-              },
-              child: Container(
-                width: double.infinity,
-                height: 50,
-                color: Colors.white12,
-                child: const Center(child: Text('InkWell')),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    duration: Duration(seconds: 5),
-                    behavior: SnackBarBehavior.floating,
-                    content: const Text('Hello from Snack Bar'),
-                    action: SnackBarAction(
-                      label: 'Undo',
-                      onPressed: () {
-                        print('Undo pressed');
-                      },
-                    ),
+              Text(nameController.text),
+              Row(
+                children: [
+                  Checkbox(
+                    tristate: true,
+                    value: isChecked,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        isChecked = value;
+                      });
+                    },
                   ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.teal,
-                foregroundColor: Colors.white,
+                  Switch(
+                    value: isSwitched,
+                    onChanged:
+                        (value) => setState(() {
+                          isSwitched = value;
+                        }),
+                  ),
+                ],
               ),
-              child: const Text('Snack Bar'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print('EvaluatedButton pressed');
-              },
-              child: const Text('EvaluatedButton'),
-            ),
-            CloseButton(),
-            BackButton()
-          ]
+              CheckboxListTile.adaptive(
+                tristate: true,
+                title: const Text('Check me'),
+                value: isChecked,
+                onChanged: (bool? value) {
+                  setState(() {
+                    isChecked = value;
+                  });
+                },
+              ),
+              SwitchListTile.adaptive(
+                title: const Text('Switch me'),
+                value: isSwitched,
+                onChanged: (bool value) {
+                  setState(() {
+                    isSwitched = value;
+                  });
+                },
+              ),
+              Slider.adaptive(
+                max: 10.0,
+                divisions: 10,
+                value: sliderValue,
+                onChanged:
+                    (value) => setState(() {
+                      sliderValue = value;
+                    }),
+              ),
+              InkWell(
+                splashColor: Colors.teal,
+                onTap: () {
+                  print('Image tapped from InkWell');
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: 50,
+                  color: Colors.white12,
+                  child: const Center(child: Text('InkWell')),
+                ),
+              ),
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          duration: Duration(seconds: 5),
+                          behavior: SnackBarBehavior.floating,
+                          content: const Text('Hello from Snack Bar'),
+                          action: SnackBarAction(
+                            label: 'Undo',
+                            onPressed: () {
+                              print('Undo pressed');
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.teal,
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text('Snack Bar'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print('EvaluatedButton pressed');
+                    },
+                    child: const Text('EvaluatedButton'),
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('EvaluatedButton pressed');
+                },
+                child: const Text('EvaluatedButton'),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
     );
   }
 }
